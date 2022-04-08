52421c5091419adb0f34d3e5e195791579a77217
Koopa343Gamer <83911090+Koopa343Gamer@users.noreply.github.com>
2022-04-08 12:44:46 +0200

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -2,8 +2,6 @@ window.alert("This site is available in my language italian. Don't worry about i
 var utente=window.prompt("Inserisci un nome. Se lasci vuoto o clicchi annulla la frase sara': Ciao! Sei nel sito per esperimenti sul mio Github.");
 if (utente == null){
   document.write("Ciao! Sei nel sito per esperimenti sul mio Github.")
-  utente = window.location.pathname
-  document.write(utente)
 }
 else{
   document.write("Ciao ")
